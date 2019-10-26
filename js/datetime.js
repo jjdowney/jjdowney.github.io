b8160be4e04a5c84beb3c7451755485057331d27
jjdowney <hud18002@byui.edu>
2019-10-26 15:35:57 -0600

    Progressive Loading

--- js/datetime.js
+++ js/datetime.js
@@ -1,3 +1,7 @@
 var year = new Date().getFullYear();
 document.getElementById('copyYear').innerHTML = year;
 document.getElementById('update').innerHTML = document.lastModified;
+
+// Load web loader
+WebFont.load({
+    google: {families: ['Roberto']}});
