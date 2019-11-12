0f8ee2549f632071776a728ac4343604bccf3eea
jjdowney <hud18002@byui.edu>
2019-11-12 12:40:29 -0700

    completed activities

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -1,4 +1,9 @@
 'use strict'
+var pageNav = document.querySelector('#page-nav');
+var statusContainer = document.querySelector('#status');
+var contentContainer = document.querySelector('#main-content');
+var locStore = window.localStorage;
+var sessStore = window.sessionStorage;
 
 // Weather Site JavaScript Functions
 document.addEventListener("DOMContentLoaded", function(){
@@ -6,16 +11,20 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log(document.getElementById("primary").classList);
     document.getElementById("primary").classList.toggle("hide");
     // Variables for Wind Chill function
-    let temp = 31;
-    let speed = 5;
-    buildWC(speed, temp);
+    // let temp = 31;
+    // let speed = 5;
+    // buildWC(speed, temp);
     // Time Indicator Function
-    let hour="1";
-    timeBall(hour);
+    // let hour="1";
+    // timeBall(hour);
     // Change Background Image
-    let outside = "Fog";
-    console.log(outside);
-    changeSummaryBackground(outside);
+    // let outside = "snow";
+    // outside = outside.toLowerCase();
+    // console.log(outside);
+    // changeSummaryBackground(outside);
+    // Get weather json data
+    let weatherURL = "../js/idahoweather.json/idahoweather.json";
+    fetchWeatherData(weatherURL);
 })
 
 // Button Function
@@ -90,7 +99,6 @@ function timeBall(hour){
     // Function to change background image
     function changeSummaryBackground(outside) {
         let x = document.getElementById('compContainer');
-        outside = outside.toLowerCase();
         console.log(`Outside is: ${outside}`);
 
     // Case Statements to change background image into current condition
@@ -120,34 +128,34 @@ function timeBall(hour){
     // Pull correct image
     let width = window.innerWidth;
     console.log(`Width of screen: ${width}`);
-    if ((width > (45)) && (outside == 'clear')) {
+    if ((width >= (720)) && (outside == 'clear')) {
         var imgLoad = "url(/images/clear-large.jpg)";
     }
-    else if ((width > (45)) && (outside == 'rain')) {
+    else if ((width >= (720)) && (outside == 'rain')) {
         var imgLoad = "url(/images/rain-large.jpg)";
     }
-    else if (width > (45) && outside == 'fog') {
+    else if (width >= (720) && outside == 'fog') {
         var imgLoad = "url(/images/fog-large.jpg)";
     }
-    else if (width > (45) && outside == 'snow') {
+    else if (width >= (720) && outside == 'snow') {
         var imgLoad = "url(/images/snow-large.jpg)";
     }
-    else if (width > (45) && outside == 'cloudy') {
+    else if (width >= (720) && outside == 'cloudy') {
         var imgLoad = "url(/images/clouds-large.jpg)";
     }
-    else if (width < (45) && outside == 'clear') {
+    else if (width < (720) && outside == 'clear') {
         var imgLoad = "url(/images/clear-small.jpg)";
     }
-    else if (width < (45) && outside == 'fog') {
+    else if (width < (720) && outside == 'fog') {
         var imgLoad = "url(/images/fog-small.jpg)";
     }
-    else if (width < (45) && outside == 'rain') {
+    else if (width < (720) && outside == 'rain') {
         var imgLoad = "url(/images/rain-small.jpg)";
     }
-    else if (width < (45) && outside == 'snow') {
+    else if (width < (720) && outside == 'snow') {
         var imgLoad = "url(/images/snow-small.jpg)";
     }
-    else if (width < (45) && outside == 'cloudy') {
+    else if (width < (720) && outside == 'cloudy') {
         var imgLoad = "url(/images/clouds-small.jpg)";
     }
     else {
@@ -160,3 +168,235 @@ function timeBall(hour){
     backgroundImg.setProperty("--dynamic-weather-background", `${imgLoad}`);
     console.log(`${imgLoad} is the new background.`);
     }
+
+    // Fetch Weather Data
+    function fetchWeatherData(weatherURL) {
+        let cityName = 'Preston'; // The data we want from the weather.json file
+        fetch(weatherURL)
+        .then(function(response) {
+            if(response.ok){
+                return response.json();
+            }
+            throw new ERROR('Network response was not OK.');
+            })
+        .then(function(data){
+        // Check the data object that was retrieved
+        console.log(data);
+        // data is the full JavaScript object, but we only want the preston part
+        // shorten the variable and focus only on the data we want to reduce typing
+        let p = data[cityName];
+
+    //**********  Get the location information  **********
+        let locName = p.properties.relativeLocation.properties.city;
+        console.log(`city is: ${locName}`);
+        let locState = p.properties.relativeLocation.properties.state;
+        console.log(`state is: ${locState}`);
+        // Put them together
+        let fullName = locName+ ', '+locState;
+        // See if it worked, using ticks around the content in the log
+        console.log(`fullName is: ${fullName}`);
+        // Get longitude and latitude and combine them to a comma separated single string
+        const latLong = p.properties.relativeLocation.geometry.coordinates[1] + ","+ p.properties.relativeLocation.geometry.coordinates[0];
+        console.log(latLong);
+        // Create a json object containing full name, latitude and longitude and store in local storage
+        const prestonData = JSON.stringify({fullName, latLong});
+        locStore.setItem("Preston,ID", prestonData);
+        
+    //**********  Get the current conditions information  **********
+        // As the data is extracted from the json, store it into session storage
+        sessStore.setItem("fullName",fullName);
+        sessStore.setItem("latLong",latLong);
+        // Get temperature data
+        let temp = p.properties.relativeLocation.properties.temperature;
+        let highTemp = p.properties.relativeLocation.properties.highTemp;
+        let lowTemp = p.properties.relativeLocation.properties.lowTemp;
+        sessStore.setItem("temp",temp);
+        sessStore.setItem("highTemp",highTemp);
+        sessStore.setItem("lowTemp",lowTemp);
+        // Get wind data
+        let windGust = p.properties.relativeLocation.properties.windGust;
+        let windSpeed = p.properties.relativeLocation.properties.windSpeed;
+        sessStore.setItem("windGust",windGust);
+        sessStore.setItem("windSpeed",windSpeed);
+        // Get hourly data using another function - should include the forecast temp, condition icons and wind speeds. The data will be stored into session storage.
+        getHourly(p.properties.forecastHourly);
+    })
+        .catch(function(error){
+            console.log('There was a fetch problem: ', error.message);
+            statusContainer.innerHTML = 'Sorry, the data could not be processed.';
+        })
+    }
+
+    // *************** Get Hourly Forecast Data ******************
+    function getHourly(URL){
+        fetch(URL)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new ERROR('Response not OK.');
+        })
+        .then(function (data) {
+        console.log('Data from getHourly function:');
+        console.log(data); //Let's see what we got back
+
+        // Store 12 hours of data to session storage
+        var hourData = [];
+        let todayDate = new Date();
+        var nowHour = todayDate.getHours();
+        console.log(`nowHour is: ${nowHour}`);
+        for (let i = 0, x = 11; i <= x; i++)
+            if (nowHour < 24) {
+                hourData[nowHour] = data.properties.periods[i].temperature + data.properties.periods[i].windSpeed + "," + data.properties.periods[i].icon;
+                sessStore.setItem(`hour${nowHour}`, hourData[nowHour]);
+                nowHour++;
+            }
+            else {
+            nowHour = nowHour - 12;
+            hourData[nowHour] = data.properties.periods[i].temperature + "," + data.properties.periods[i].windSpeed + "," + data.properties.periods[i].icon;
+            sessStore.setItem(`hour${nowHour}`, hourData[nowHour]);
+            nowHour = 1;
+            }
+
+            // Get the shortForecast value from the first hour (the current hour)
+            // This will be the condition keyword for setting the background image
+            sessStore.setItem('shortForecast', data.properties.periods[0].shortForecast);
+
+            // Call the buildPage function
+            buildPage();
+        })
+            .catch(error => console.log('There was a getHourly error: ', error));
+    }
+
+    // *********************** Build the Weather Page ************************************
+    function buildPage() {
+        // Set the title with the location name at the first
+        // Gets the title element so it can be worked with
+        let pageTitle = document.querySelector('#page-title');
+        // Create a text node containing the full name
+        let fullNameNode = document.createTextNode(sessStore.getItem('fullName'));
+        // inserts the fullName value before any other content that might exist
+        pageTitle.insertBefore(fullNameNode, pageTitle.childNodes[0]);
+        // When this is done the title should look like this:
+        // Preston, Idaho | mybetterweather.com
+    }
+
+    // Get the h1 to display the city location
+    let contentHeading = document.querySelector('#contentHeading');
+    contentHeading.innerHTML = sessStore.getItem('fullName');
+    // The h1 in the main element should now say "Preston, Idaho"
+
+    // Get the coordinates container for the location
+    let latLon = document.querySelector('#location');
+    latLon.innerHTML = sessStore.getItem('latLong');
+    // The latitude and longitude should match what was stored in session storage
+
+    // Get the condition keyword and set Background picture
+    changeSummaryBackground(sessStore.getItem('shortForecast'));
+
+    // ************ Set Current Conditions Info **************************
+    // Set the temperature information
+    let highTemp = document.querySelector('#high');
+    let loTemp = document.querySelector('#low');
+    let currentTemp = document.querySelector('#temp');
+    let feelTemp = document.querySelector('#feelTemp');
+    highTemp.innerHTML = sessStore.getItem('highTemp') + "°F";
+    loTemp.innerHTML = sessStore.getItem('lowTemp') + "°F";
+    currentTemp.innerHTML = sessStore.getItem('temperature') + "°F";
+    // Set wind information
+    let speed = document.querySelector('#windSpeed');
+    let gust = document.querySelector('#gusts');
+    speed.innerHTML = sessStore.getItem('windSpeed');
+    gust.innerHTML = sessStore.getItem('windGust');
+    // Calculate feel like temp
+    feelTemp.innerHTML = buildWC(sessStore.getItem('windSpeed'), sessStore.getItem('temperature')) + "°F";
+
+    // ******** Set Time Indicators **************
+    let thisDate = new Date();
+    var currentHour = thisDate.getHours();
+    let indicatorHour;
+    // if Hour is greater than 12, subtract 12
+    if (currentHour > 12) {
+        indicatorHour = currentHour - 12;
+    }
+    else {
+        indicatorHour = currentHour;
+    };
+    console.log(`Current hour in time indicator is: ${currentHour}`);
+    // Set time indicator
+    timeBall(indicatorHour);
+
+    // ****************** Hourly Temperature Component *************
+    // Get the hourly data from storage as an array
+    let currentData = [];
+    let tempHour = currentHour;
+    // Adjust counter based on current time
+    for (let i = 0, x = 12; i < x; i++) {
+        if (tempHour <= 23) {
+            currentData[i] = sessStore.getItem("hour" + tempHour).split(", ");
+            tempHour++;
+        }
+        else {
+            tempHour = tempHour - 12;
+            currentData[i] = sessStore.getItem("hour" + tempHour).split(", ");
+            console.log(`CurrentData[i][0] is: ${currentData[i][0]}`);
+            tempHour = 1;
+        } 
+    }
+    console.log(`currentData is: ${currentData}`);
+
+    // Loop through array inserting data
+    // Start with the outer container that matches the current time
+    tempHour = currentHour;
+    for (let i = 0, x = 12; i < x; i++) {
+        if (tempHour >= 13) {
+            tempHour = tempHour - 12;
+        }
+        console.log(`Start container is: #temp o.${tempHour}`);
+        document.querySelector('.temperature .o' + tempHour).innerHTML = currentData[i][0];
+        tempHour++;
+    }
+// *************** Hourly Wind Component *************************
+// Get hourly data from storage
+let windArray = [];
+let windHour = currentHour;
+// Adjust counter based on current time
+for (let i = 0, x = 12; i < x; i++) {
+    if (windHour <= 23) {
+        windArray[i] = currentData[i][1].split(", ");
+        console.log(`windArray[i] is: ${windArray}`);
+        windHour++;
+    }
+    else {
+        windHour = windHour - 12;
+        windArray[i] = currentData[i][1].split(", ");
+        windHour = 1;
+    }
+}
+console.log(windArray);
+
+// Insert Wind data
+// Start with the outer container that matches the time indicator
+windHour = currentHour;
+for (let i = 0, x = 12; i < x; i++) {
+    if (windHour >= 13) {
+        windHour = windHour - 12;
+    }
+    document.querySelector('.wind .o' + windHour).innerHTML = windArray[i][0];
+    windHour++;
+}
+
+// ******************** Condition Component Icons ***************
+let conditionHour = currentHour;
+// Adjust counter based on current tiem
+for (let i = 0, x = 12; i < x; i++) {
+    if (conditionHour >= 13) {
+        conditionHour = conditionHour - 12;
+    }
+    document.querySelector('.conditions .o' + conditionHour).innerHTML = '<img src=\"' + currentData[i][2] + '\" alt="hourly weather condition image">';
+    conditionHour++;
+}  
+
+// Change the status of the containers
+contentContainer.setAttribute('class', ''); // removes the hide class from main
+statusContainer.setAttribute('class', 'hide'); // hides the status container
