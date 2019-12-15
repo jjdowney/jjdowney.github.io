9c5563064781fe22f4e14a2963eac985a0486594
jjdowney <hud18002@byui.edu>
2019-12-14 18:03:23 -0700

    finished

--- temple-inn/js/buttons.js
+++ temple-inn/js/buttons.js
@@ -1,4 +1,9 @@
+var sessStore = window.sessionStorage;
+let URL = "/temple-inn/js/temple-info.json";
+templeClosures(URL);
 footerDate();
+
+
 // Copyright Year
 var year = new Date().getFullYear();
 document.getElementById('copyYear').innerHTML = year;
@@ -31,6 +36,9 @@ function toggleMenu() {
     document.getElementById("primary").classList.toggle("hide");
 }
 
+
+
+
 /*************Collapsible Buttons************************/
 var coll = document.getElementsByClassName("collapsible");
 var i;
@@ -45,4 +53,95 @@ for (i = 0; i < coll.length; i++) {
       content.style.display = "block";
     }
   });
+}
+
+
+function templeClosures(URL) {
+    fetch(URL)
+    .then(function(response) {
+        if (response.ok){
+            return response.json();
+        }
+        throw new ERROR('Response not OK.');
+    })
+    .then(function(data){
+        console.log(data);
+
+        let sanDiego = (data.sanDiego.closures);
+        let sd1 = sanDiego[0] + "<br>";
+        let sd2 = sanDiego[1] + "<br>";
+        let sd3 = sanDiego[2] + "<br>";
+        let sd4 = sanDiego[3] + "<br>";
+        let sd5 = sanDiego[4] + "<br>";
+        let sd6 = sanDiego[5] + "<br>";
+        let sd7 = sanDiego[6] + "<br>";
+        let sd8 = sanDiego[7] + "<br>";
+        let sd9 = sanDiego[8] + "<br>";
+        let sd10 = sanDiego[9] + "<br>";
+        let sd11 = sanDiego[10] + "<br>";
+        sanDiegoList = sd1 + '\n' + sd2 + '\n'+ sd3 + '\n' + sd4 + '\n' + sd5 + '\n' + sd6 + '\n' + sd7 + '\n' + sd8 + '\n' + sd9 + '\n' + sd10 + '\n' + sd11;
+        sessStore.setItem('sanDiegoList', sanDiegoList);
+        console.log(`San Diego List is: ${sanDiegoList}`);
+  
+        let paris = (data.paris.closures);
+        let pf1 = paris[0] + "<br>";
+        let pf2 = paris[1] + "<br>";
+        let pf3 = paris[2] + "<br>";
+        let pf4 = paris[3] + "<br>";
+        let pf5 = paris[4] + "<br>";
+        let pf6 = paris[5] + "<br>";
+        let pf7 = paris[6] + "<br>";
+        let pf8 = paris[7] + "<br>";
+        parisList = pf1 + '\n' + pf2 + '\n'+ pf3 + '\n' + pf4 + '\n' + pf5 + '\n' + pf6 + '\n' + pf7 + '\n' + pf8;
+        sessStore.setItem('parisList', parisList);
+        console.log(`Paris List is: ${parisList}`);
+
+        let boston = (data.boston.closures);
+        let bm1 = boston[0] + "<br>";
+        let bm2 = boston[1] + "<br>";
+        let bm3 = boston[2] + "<br>";
+        let bm4 = boston[3] + "<br>";
+        let bm5 = boston[4] + "<br>";
+        let bm6 = boston[5] + "<br>";
+        let bm7 = boston[6] + "<br>";
+        let bm8 = boston[7] + "<br>";
+        let bm9 = boston[8] + "<br>";
+        let bm10 = boston[9] + "<br>";
+
+        bostonList = bm1 + '\n' + bm2 + '\n'+ bm3 + '\n' + bm4 + '\n' + bm5 + '\n' + bm6 + '\n' + bm7 + '\n' + bm8 + '\n' + bm9 + '\n' + bm10;
+        sessStore.setItem('bostonList', bostonList);
+        console.log(`Boston List is: ${bostonList}`);
+
+        let manaus = (data.manaus.closures);
+        let mb1 = manaus[0] + "<br>";
+        let mb2 = manaus[1] + "<br>";
+        let mb3 = manaus[2] + "<br>";
+        let mb4 = manaus[3] + "<br>";
+        let mb5 = manaus[4] + "<br>";
+        let mb6 = manaus[5] + "<br>";
+        let mb7 = manaus[6] + "<br>";
+        let mb8 = manaus[7] + "<br>";
+
+        manausList = mb1 + '\n' + mb2 + '\n'+ mb3 + '\n' + mb4 + '\n' + mb5 + '\n' + mb6 + '\n' + mb7 + '\n' + mb8;
+        sessStore.setItem('manausList', manausList);
+        console.log(`Manaus List is: ${manausList}`);
+
+        postClosures();
+    })
+    .catch(error => console.log("There was a fetch problem: ", error));
+}
+
+function postClosures() {
+let sDClosures = document.querySelector('#sdclosures');
+sDClosures.innerHTML = sessStore.getItem('sanDiegoList');
+
+let pFClosures = document.querySelector('#pfclosures');
+pFClosures.innerHTML = sessStore.getItem('parisList');
+
+let bMClosures = document.querySelector('#bmclosures');
+bMClosures.innerHTML = sessStore.getItem('bostonList');
+
+let mBClosures = document.querySelector('#mbclosures');
+mBClosures.innerHTML = sessStore.getItem('manausList');
+
 }
--- temple-inn/js/reservation.js
+++ temple-inn/js/reservation.js
@@ -31,49 +31,6 @@ function toggleMenu() {
     document.getElementById("primary").classList.toggle("hide");
 }
 
-// //Form Data
-
-// document.addEventListener("DOMContentLoaded", ()=>{
-//   document.querySelector('#submit').addEventListener("click", processData);
-// })
-
-// let reservations = [];
-// let processData = (event) => {
-//   // stop the form from submitting
-//   event.preventDefault();
-//   let reservation = {
-//   name: document.querySelector('#name').value,
-//   guests: document.querySelector('#guests').value,
-//   startDate: document.querySelector('#start').value,
-//   endDate: document.querySelector('#end').value,
-//   temple: document.querySelector('#temple').value,
-//   email: document.querySelector('#email').value,
-//   number: document.querySelector('#number').value,
-//   message: document.querySelector('#message').value
-//   }
-// // adds reservation to the end of the array of all reservations
-// reservations.push(reservation);
-// // reset the first, and only, form
-// document.forms[0].reset;
-// // see results in console
-// console.log('newRes', {reservations});
-
-
-//   // Store to session Storage
-//   window.sessionStorage.setItem("reservation", JSON.stringify(reservations));
-//   // Retrieve from session storage
-//   let resList = JSON.parse(window.sessionStorage.getItem("reservations"));
-//   console.log(resList);
-  
-// //   // inject to the page
-// // const resDetails = document.querySelector("#resResult pre");
-// // resDetails.textContent = "\n" + JSON.stringify(reservations, "\t", 2);
-  
-// // // display the results
-// // document.querySelector("#resResult").classList.remove("hide");
-// let fullName = document.querySelector('#n');
-// fullName.innerHTML = sessStore.getItem('name');
-// }
 
 document.addEventListener("DOMContentLoaded", ()=>{
   document.querySelector('#submit').addEventListener("click", processData);
@@ -108,11 +65,25 @@ console.log('newRes', {reservations});
   console.log(resList);
   
   // inject to the page
-const resDetails = document.querySelector("#resResult pre");
-resDetails.textContent = "\n" + JSON.stringify(reservations, "\t", 2);
+  let name = 'Full Name: ' + resList[0].name + '<br>';
+  let guests = 'Number of Guests: ' + resList[0].guests + '<br>'
+  let start = 'Reservation Start Date: ' + resList[0].startDate + '<br>';
+  let end = 'Reservation End Date: ' + resList[0].endDate + '<br>';
+  let temple = 'Temple Location: ' + resList[0].temple + '<br>';
+  let email = 'Email: ' + resList[0].email + '<br>';
+  let phone = 'Phone Number: ' + resList[0].number + '<br>';
+  let message = 'Special Requests: ' + resList[0].message + '<br>';
+  
+  let summary = name + guests + start + end + temple + email + phone + message;
+
+  const resDetails = document.querySelector("#resResult div");
+  resDetails.innerHTML = summary;
+  // "\n" + JSON.stringify(reservations, "\t", 2);
   
 // display the results
-document.querySelector("#resResult").classList.remove("hide");
+  document.querySelector("#form").classList.add("hide");
+  document.querySelector("#resResult").classList.remove("hide");
+
 
 }
 
